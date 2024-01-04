37360c3bf3c294057815fb20ad8802ac9e338cfb
MichaelVita <michalisvita@mhnyma.com>
2024-01-04 14:50:24 +0200

    Fixed topdown

--- parser/TopDown.hs
+++ parser/TopDown.hs
@@ -1,10 +1,37 @@
 import Types
+import Matcher
 
 
 
-topDown :: [Clause] -> Pred -> Bool
-topDown (clause:clauses) query
+topDown :: [Clause] -> Pred -> Pred
+topDown clauses pred = topDownHelper clauses clauses pred
+
+topDownHelper :: [Clause] -> [Clause] -> Pred -> Maybe Pred
+topDownHelper _ [] _ = Nothing
+topDownHelper allClauses (clause:clauses) pred =
     case clause of
-        CFact pred -> if matchPred query pred then True else topDown query clauses
-        CRule pred preds -> if matchPred query pred then all (topDown query) preds else topDown query clauses
+        CFact fact ->
+            case matchPred fact pred of
+                Just assignment -> Just $ applyAssignmentPred assignment pred
+                Nothing -> topDownHelper allClauses clauses pred
+        CRule ruleHead ruleTail -> 
+            case  matchPred ruleHead pred of
+                Just assignment -> 
+                    let newPred = applyAssignmentPred assignment pred in
+                    case matchPredToList newPred ruleTail of
+                        Just assignment2 -> Just $ applyAssignmentPred assignment2 newPred
+                        Nothing -> topDownHelper allClauses clauses pred
+                    
+                Nothing -> topDownHelper allClauses clauses pred
 
+
+matchPredToList :: Pred -> [Pred] -> Maybe [Assignment]
+matchPredToList pred [] = Just []
+matchPredToList pred (p:ps) = 
+    case matchPred pred p of
+        Nothing -> Nothing
+        Just assignment -> 
+            let newPred = applyAssignmentPred assignment pred in
+            case matchPredToList newPred ps of
+                Nothing -> Nothing
+                Just assignment2 -> Just (composeAssignments assignment assignment2)
